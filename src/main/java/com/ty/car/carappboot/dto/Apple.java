1b5c447e3f011d13c87540c7da66f263123549e2
prakashjambage <jambageprakash23@gmail.com>
2022-03-28 14:44:17 +0530

    adding apple class

---
+++ src/main/java/com/ty/car/carappboot/dto/Apple.java
@@ -0,0 +1,12 @@
+package com.ty.car.carappboot.dto;
+
+import org.jboss.jandex.Main;
+
+public class Apple {
+public static void main(String[] args) {
+	System.out.println("from apple");
+}
+	
+	
+
+}
---
+++ src/main/java/com/ty/car/carappboot/dto/Orange.java
@@ -0,0 +1,7 @@
+package com.ty.car.carappboot.dto;
+
+public class Orange {
+public static void main(String[] args) {
+	System.out.println("orange");
+}
+}
